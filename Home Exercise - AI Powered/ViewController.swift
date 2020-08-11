a18237e9890de39d4464f8dd637a0ec9d16cf370
Artem Sherbachuk <artemsherbachuk@gmail.com>
2020-08-11 12:18:27 +0200

    Initial Commit

---
+++ Home Exercise - AI Powered/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Exercise - AI Powered
+//
+//  Created by Artem Sherbachuk on 8/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
